4ee6d18e8680903e984e09f83a21619e6ad25428
David Bruant <davidbruant@protonmail.com>
2020-08-09 17:31:30 +0200

    First shot at integrating svelte and all

---
+++ scripts/components/BigAddScreen.svelte
@@ -0,0 +1,16 @@
+<main>
+    <section class="add">
+        <strong>Rajouter un lieu</strong>
+        <a>Ici</a>
+    </section>
+    <section class="list">
+        <a>Voir la liste</a>
+    </section>
+</main>
+
+<style>
+	main {
+		
+	}
+</style>
+
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import BigAddScreen from './components/BigAddScreen.svelte';
+
+const app = new BigAddScreen({
+	target: document.body,
+});
+
+export default app;
